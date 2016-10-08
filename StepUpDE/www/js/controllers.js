c9b41e2ebf7f8e7c2f8b2c1d9c7693c45a1b955d
Carina Blair <carinablair@Carina-ZCW-5.local>
2016-10-08 10:45:27 -0400

    #BestCodeEver layout done

--- StepUpDE/www/js/controllers.js
+++ StepUpDE/www/js/controllers.js
@@ -52,14 +52,34 @@ function ($scope, $stateParams) {
 
 }])
 
+
 .controller('signUpCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $http, $stateParams) {
+ $scope.submit = function(data){
+   var username = document.getElementById('signUp-input5');
+   var password = document.getElementById('signUp-input6');
+   var zipcode =document.getElementById('signUp-input7')
+   alert("Thanks for Signing up with United WAY!");
+   var dataObj = {
+     username : username.value,
+     password: password.value,
+     zipcode : zipcode.value
+   };
+   $http.post("http://localhost:8080/signup", dataObj)
+   .then(
+     function(response){
+       console.log(response);
+     },
+     function(response){
+       console.log(response);
+     }
+   );
+ }
 
 
 }])
-
 .controller('settingsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
