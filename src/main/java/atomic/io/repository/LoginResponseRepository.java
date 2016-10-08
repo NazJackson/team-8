850a3e0369be4465d4f926cbde5ee0bdb0004f21
Nazhir Jackson <nazhirjackson@Nazhir-ZCW-23.local>
2016-10-07 22:16:00 -0400

    added a getter

--- src/main/java/atomic/io/repository/LoginResponseRepository.java
+++ src/main/java/atomic/io/repository/LoginResponseRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Created by nazhirjackson on 10/7/16.
  */
 public interface LoginResponseRepository extends JpaRepository<LoginResponse, Long>{
+
 }
